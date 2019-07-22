48353d44041ca702b766f8b64360cbae814a94b8
GianIsNotDead <gianisalive@gmail.com>
2019-07-21 19:58:11 -0700

    remove source map for production build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,7 +5,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: 'source-map',
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new webpack.DefinePlugin({
