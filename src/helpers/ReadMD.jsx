5fd4b19a2754a0d448fa6df51497760aeeed5fa3
GianIsNotDead <gianisalive@gmail.com>
2020-09-29 16:12:21 -0700

    update

--- src/helpers/ReadMD.jsx
+++ src/helpers/ReadMD.jsx
@@ -13,12 +13,11 @@ class ReadMD {
     this.components = [];
   }
   parse() {
-    console.log('parsing...');
     let pos = 0;
     while(pos < this.mdString.length) {
       let strLength = null;
       // header
-      if (this.mdString[pos] === "#" && (this.mdString[pos - 1].concat(this.mdString[pos])) !== '\#') {
+      if (this.mdString[pos] === "#") {
         strLength = this._makeHeader(this.mdString.substr(pos));
       }
       // image
@@ -38,12 +37,32 @@ class ReadMD {
     return this.components;
   }
   _makeHeader(mdString) {
-    console.log('header detected');
-    return 2;
+    let header = { type: 1, text: '' };
+    let strPos = 0;
+    let done = false;
+    while (done === false) {
+      if (
+        (strPos > 3 && mdString[strPos] === '#')
+        || mdString[strPos].concat(mdString[strPos + 1]).match(/\n/g) !== null
+        || mdString[strPos].concat(mdString[strPos + 1]) === '!['
+        || mdString[strPos] === '['
+      ) {
+        console.log('mdString: ', mdString[strPos].concat(mdString[strPos + 1]));
+        done = true;
+      }
+      else if ((strPos > 0 && strPos < 4) && mdString[strPos] === '#') {
+        header.type = header.type + 1;
+        strPos = strPos + 1;
+      }
+      else {
+        header.text = header.text + mdString[strPos];
+        strPos = strPos + 1;
+      }
+    }
+    this.components.push(header);
+    return strPos;
   }
   _makeImg(mdString) {
-    console.log('making image...... ');
-    console.log('mdString: ', mdString);
     let image = { alt: '', src: '' };
     let currentAttr = 'alt';
     let strPos = 0;
@@ -61,38 +80,52 @@ class ReadMD {
       strPos = strPos + 1;
     }
     this.components.push(image);
-    console.log('image: ', image);
     return strPos;
   }
   _makeLink(mdString) {
-    console.log('link detected');
-    return 2;
+    let link = { text: '', href: '' };
+    let currentAttr = 'text';
+    let strPos = 0;
+    let done = false;
+    while (done === false) {
+      if (mdString[strPos] === ')') {
+        done = true;
+      }
+      else if (mdString[strPos] === ']') {
+        currentAttr = 'href';
+      }
+      else if (mdString[strPos].match(/\[|\(/g) === null) {
+        link[currentAttr] = link[currentAttr] + mdString[strPos];
+      }
+      strPos = strPos + 1;
+    }
+    this.components.push(link);
+    return strPos;
   }
   _makeParagraph(mdString) {
-    console.log('making paragraph...');
-    let paragraph = null;
+    let paragraph = '';
     let strPos = 0;
-    let foundSyntax = false;
-    while (foundSyntax === false) {
+    let done = false;
+    while (done === false) {
       if (
         mdString[strPos] === '#'
         || mdString[strPos].concat(mdString[strPos + 1]) === '!['
         || mdString[strPos] === '['
       ) {
-        foundSyntax = true;
-      } else {
+        done = true;
+      }
+      else {
         paragraph = paragraph + mdString[strPos];
         strPos = strPos + 1;
       }
     }
     // this.components.push(<p className={this.classes.p}>{paragraph}</p>);
     this.components.push(paragraph);
-    console.log('paragraph: ', paragraph);
     return strPos;
   }
 }
 
-let sampleText = "No one really should, but if you're relatively new to electronics, software, and neuroscience, and want to learn more by building, then this might be a thing for you. You’ll have a chance to explore SPI communication interface, noise filtering, and intricacies of Operational Amplifier (OpAmp) and Analog to Digital Converter (ADC). ![some image](../someImage.com) If you're just looking for an affordable EEG to run experiments, there are better ones out there, such as [OpenBCI](https://openbci.com)";
+let sampleText = "##This is Header \nNo one really should, but if you're relatively new to electronics, software, and neuroscience, and want to learn more by building, then this might be a thing for you. You’ll have a chance to explore SPI communication interface, noise filtering, and intricacies of Operational Amplifier (OpAmp) and Analog to Digital Converter (ADC). ![some image](../someImage.com) If you're just looking for an affordable EEG to run experiments, there are better ones out there, such as [OpenBCI](https://openbci.com)";
 
 new ReadMD(sampleText).parse();
 
